d73607fc6696be76d0f23227e68661c7bfb19e9b
Lark <lark@Larks-MBP.home>
2025-09-21 16:49:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";  
+import axios from "axios";               
 
 function App() {
+  const [file, setFile] = useState(null);  
+  const [text, setText] = useState("");   
+
+  const handleUpload = async () => {
+    const formData = new FormData();       
+    formData.append("file", file);         
+
+    const res = await axios.post("https://pdf-to-text-tg44.onrender.com/upload", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    setText(res.data.text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>PDF to Text</h1>
+
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+
+      <button onClick={handleUpload}>Upload</button>
+
+      <pre>{text}</pre>
     </div>
   );
 }
